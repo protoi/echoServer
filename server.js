3c2241e26c56fdb145dc2ab83857697a81da8650
protoi <protikdatta9@gmail.com>
2023-01-17 12:46:27 +0530

    vanilla server

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require("http")
+
+var server  = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' }); 
+    res.write("Hello World");
+    res.end();
+});
+
+server.listen(9999);
+console.log("web server running at http://localhost:9999");
